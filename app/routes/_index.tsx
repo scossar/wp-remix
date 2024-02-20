1e50eb7465dd7e7925bdf36c833b71bc79fba5ef
scossar <scossar3@gmail.com>
2024-02-20 12:32:09 -0800

    Add some text

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,6 +11,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Hello World!</h1>
+      <p>This is a test, this is only a test...</p>
     </div>
   );
 }
