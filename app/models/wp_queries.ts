174956a53d3b770ccafbbc04ce74b6353a9d1ea8
scossar <scossar3@gmail.com>
2024-02-24 20:02:08 -0800

    Pull in latest posts from WordPress

--- app/models/wp_queries.ts
+++ app/models/wp_queries.ts
@@ -27,3 +27,61 @@ query GetPosts {
     }
 }
 `);
+
+export const INDEX_PAGE_POSTS_QUERY = gql(`
+query getSitePosts {
+    posts(first: 5, where: {orderby: {field: DATE, order: DESC}}) {
+      edges {
+        node {
+          id
+          title
+          excerpt
+          date
+          author {
+            node {
+              name
+            }
+          }
+          featuredImage {
+            node {
+              caption
+              description
+              id
+              sourceUrl
+            }
+          }
+        }
+      }
+    }
+    categories {
+      edges {
+        node {
+          name
+          posts(first: 5, where: {orderby: {field: DATE, order: DESC}}) {
+            edges {
+              node {
+                id
+                title
+                excerpt
+                date
+                author {
+                  node {
+                    name
+                  }
+                }
+                featuredImage {
+                  node {
+                    caption
+                    description
+                    id
+                    sourceUrl
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`);
--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,31 +1,49 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+import { createApolloClient } from "lib/createApolloClient";
+import type { PostConnectionEdge } from "~/graphql/__generated__/graphql";
+import { INDEX_PAGE_POSTS_QUERY } from "~/models/wp_queries";
 import PostExcerptCard from "~/components/PostExcerptCard";
 
-const sections = [
-  "Latest Posts",
-  "Web Development",
-  "Modular",
-  "Ride Logs",
-  "Definitions",
-  "Zalgorithm",
-];
-const iterations = Array(5).fill(0);
+export async function loader() {
+  const client = createApolloClient();
+  const response = await client.query({
+    query: INDEX_PAGE_POSTS_QUERY,
+  });
+
+  if (response.errors) {
+    // todo: handle the error
+    console.log(
+      "An unhandled error was returned from the INDEX_PAGE_POSTS_QUERY"
+    );
+  }
+
+  // if this isn't set, an error should already have been thrown
+  const data = response?.data;
+  const latestPostsEdges = data?.posts?.edges;
+  const categoryPostsEdges = data?.categories?.edges;
+
+  return json({
+    latestPostsEdges: latestPostsEdges,
+    categoryPostsEdges: categoryPostsEdges,
+  });
+}
 
 export default function Blog() {
+  const { latestPostsEdges, categoryPostsEdges } =
+    useLoaderData<typeof loader>();
   return (
     <div className="px-6 mx-auto">
-      {sections.map((section) => (
-        <div key={section}>
-          <h2 className="text-2xl text-slate-900 mt-3 font-serif font-bold">
-            {section}
-          </h2>
-          <hr className="my-3 border-solid border-slate-900" />
-          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-            {iterations.map((_, index) => (
-              <PostExcerptCard key={index} />
-            ))}
-          </div>
-        </div>
-      ))}
+      <h2 className="text-2xl text-slate-900 mt-3 font-serif font-bold">
+        Latest Posts
+      </h2>
+      <hr className="my-3 border-solid border-slate-900" />
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
+        {latestPostsEdges.map((edge: PostConnectionEdge) => (
+          <PostExcerptCard postConnectionEdge={edge} key={edge.node.id} />
+        ))}
+      </div>
     </div>
   );
 }
