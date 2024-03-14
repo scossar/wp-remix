646517eb239f9968307fb4d4279ce4f286016eb2
scossar <scossar3@gmail.com>
2024-03-14 02:32:37 -0700

    change buttons to arrows

--- app/components/Paginator.tsx
+++ app/components/Paginator.tsx
@@ -1,5 +1,6 @@
 import { Link } from "@remix-run/react";
 import type { RootQueryToPostConnectionPageInfo } from "~/graphql/__generated__/graphql";
+import { Icon } from "~/components/Icon";
 
 interface PaginatorProps {
   pageInfo: RootQueryToPostConnectionPageInfo;
@@ -9,27 +10,35 @@ export default function Paginator({ pageInfo }: PaginatorProps) {
   const { hasNextPage, hasPreviousPage, startCursor, endCursor } = pageInfo;
 
   return (
-    <div>
-      {hasPreviousPage && startCursor ? (
-        <Link
-          to={`?startCursor=${startCursor}`}
-          className="mr-6 px-3 py-2  bg-sky-700 hover:bg-sky-600 text-slate-50 rounded"
-        >
-          Previous
-        </Link>
-      ) : (
-        ""
-      )}
-      {hasNextPage && endCursor ? (
-        <Link
-          to={`?endCursor=${endCursor}`}
-          className="ml-6 px-3 py-2 bg-sky-700 hover:bg-sky-600 text-slate-50 rounded"
-        >
-          Next
-        </Link>
-      ) : (
-        ""
-      )}
+    <div className="w-full flex justify-between">
+      <div className="flex">
+        {hasPreviousPage && startCursor ? (
+          <Link to={`?startCursor=${startCursor}`}>
+            <Icon
+              key="arrow-left"
+              id="arrow-left"
+              className="text-slate-700 w-10 h-10"
+            />
+            <div>Previous Posts</div>
+          </Link>
+        ) : (
+          ""
+        )}
+      </div>
+      <div className="flex">
+        {hasNextPage && endCursor ? (
+          <Link to={`?endCursor=${endCursor}`}>
+            <Icon
+              key="arrow-right"
+              id="arrow-right"
+              className="text-slate-700 w-10 h-10 justify-end"
+            />
+            <div>Next Posts</div>
+          </Link>
+        ) : (
+          ""
+        )}
+      </div>
     </div>
   );
 }
