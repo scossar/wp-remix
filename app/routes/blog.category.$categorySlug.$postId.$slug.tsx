e79312010241b99a7dc3edea2d43ddfff0d6366b
scossar <scossar3@gmail.com>
2024-03-15 17:11:23 -0700

    move previous/next post navigation to a component

--- app/routes/blog.category.$categorySlug.$postId.$slug.tsx
+++ app/routes/blog.category.$categorySlug.$postId.$slug.tsx
@@ -7,7 +7,8 @@ import { createApolloClient } from "lib/createApolloClient";
 import { CATEGORY_POST_BY_ID_QUERY } from "~/models/wp_queries";
 import type { Post } from "~/graphql/__generated__/graphql";
 import { stripHtml, truncateText } from "~/utils/utilities";
-import { Icon } from "~/components/Icon";
+import type { PostNavigationProps } from "~/components/OlderNewerPostNavigation";
+import OlderNewerPostNavigation from "~/components/OlderNewerPostNavigation";
 
 interface LoaderData {
   post: Post;
@@ -99,12 +100,12 @@ export default function BlogPost() {
         "0"
       )}`
     : "";
-  const previousTitle = post?.previousPostInCategory?.title;
-  const previousSlug = post?.previousPostInCategory?.slug;
-  const previousId = post?.previousPostInCategory?.databaseId;
-  const nextTitle = post?.nextPostInCategory?.title;
-  const nextSlug = post?.nextPostInCategory?.slug;
-  const nextId = post?.nextPostInCategory?.databaseId;
+
+  const postNavigationProps: PostNavigationProps = {
+    previousPost: post?.previousPostInCategory,
+    nextPost: post?.nextPostInCategory,
+    basePath: categorySlug ? `/blog/category/${categorySlug}` : "/blog",
+  };
 
   return (
     <div className="mx-2 my-6 md:max-w-prose md:mx-auto">
@@ -141,49 +142,7 @@ export default function BlogPost() {
         className="text-slate-800 wp-post"
         dangerouslySetInnerHTML={{ __html: post.content }}
       />
-      <div className="my-3 flex justify-between  flex-col min-[431px]:flex-row">
-        {previousTitle && previousSlug && previousId ? (
-          <div>
-            <Link
-              prefetch="intent"
-              to={`/blog/category/${categorySlug}/${previousId}/${previousSlug}`}
-              className="text-lg font-bold text-sky-700 hover:underline"
-            >
-              <div className="flex items-center">
-                {" "}
-                <Icon
-                  key="arrow-left"
-                  id="arrow-left"
-                  x={-5}
-                  className="text-slate-700 w-10 h-10 self-center"
-                />{" "}
-                <div className="text-slate-700">Older</div>
-              </div>
-              {previousTitle}
-            </Link>
-          </div>
-        ) : null}
-        {nextTitle && nextSlug && nextId ? (
-          <div className="min-[431px]:text-right">
-            <Link
-              prefetch="intent"
-              to={`/blog/category/${categorySlug}/${nextId}/${nextSlug}`}
-              className="text-lg font-bold text-sky-700 hover:underline"
-            >
-              <div className="flex items-center min-[431px]:justify-end">
-                <div className="text-slate-700">Newer</div>
-                <Icon
-                  key="arrow-right"
-                  id="arrow-right"
-                  x={5}
-                  className="text-slate-700 inline w-10 h-10"
-                />{" "}
-              </div>
-              {nextTitle}
-            </Link>
-          </div>
-        ) : null}
-      </div>
+      <OlderNewerPostNavigation {...postNavigationProps} />
     </div>
   );
 }
